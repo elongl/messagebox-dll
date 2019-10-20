2232a2e81c8e4e4197df52e539891fbef446d2eb
Elon Gliksberg <elongliks@gmail.com>
2019-10-20 19:15:27 +0300

    Removed redundant call to CreateThread.

--- MessageBoxDll/dllmain.cpp
+++ MessageBoxDll/dllmain.cpp
@@ -1,16 +1,11 @@
 #include <Windows.h>
 #include "pch.h"
 
-void ShowMessageBox()
-{
-	MessageBoxA(NULL, "Hello World!", "Hello There!", NULL);
-}
-
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
 {
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH: CreateThread(NULL, NULL, (LPTHREAD_START_ROUTINE)ShowMessageBox, NULL, NULL, NULL);
+	case DLL_PROCESS_ATTACH: MessageBoxA(NULL, "Hello World!", "Hello There!", NULL);
 	case DLL_THREAD_ATTACH: break;
 	case DLL_THREAD_DETACH: break;
 	case DLL_PROCESS_DETACH: break;
