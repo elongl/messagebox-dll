f8560e102b90e305e62bafdd59965975711f1e8b
Elon Gliksberg <elongliks@gmail.com>
2019-10-20 19:26:17 +0300

    Revert "Popping the messagebox on thread attachment, rather than process, so we can inject multiple times."
    This reverts commit e0f156c12221dffb04ba49b12285a70f42b6466e.

--- MessageBoxDll/dllmain.cpp
+++ MessageBoxDll/dllmain.cpp
@@ -5,8 +5,8 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReser
 {
 	switch (ul_reason_for_call)
 	{
-	case DLL_PROCESS_ATTACH: break;
-	case DLL_THREAD_ATTACH: MessageBoxA(NULL, "Hello World!", "Hello There!", NULL);
+	case DLL_PROCESS_ATTACH: MessageBoxA(NULL, "Hello World!", "Hello There!", NULL);
+	case DLL_THREAD_ATTACH: break;
 	case DLL_THREAD_DETACH: break;
 	case DLL_PROCESS_DETACH: break;
 	}
